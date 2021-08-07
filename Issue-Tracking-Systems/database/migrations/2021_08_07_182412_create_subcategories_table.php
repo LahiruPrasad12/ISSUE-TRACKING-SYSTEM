3f46c4d8a714a784fa040940b55a5bbffe30c2bc
Oshada1988 <lahirupr471@gmail.com>
2021-08-07 23:57:37 +0530

    create subcategory model

---
+++ Issue-Tracking-Systems/database/migrations/2021_08_07_182412_create_subcategories_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateSubcategoriesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('subcategories', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('name');
+            $table->text('description');
+            $table->bigInteger('category_Id')->unsigned();
+            $table->foreign('category_Id')->references('id')->on('categories')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('subcategories');
+    }
+}
