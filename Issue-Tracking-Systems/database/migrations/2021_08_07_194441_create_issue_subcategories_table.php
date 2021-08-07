dd7d9e94b99c5be4fe25f1b6de8663ebe281cf2f
Oshada1988 <lahirupr471@gmail.com>
2021-08-08 01:16:53 +0530

    create issue__subcategories model

---
+++ Issue-Tracking-Systems/database/migrations/2021_08_07_194441_create_issue_subcategories_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateIssueSubcategoriesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('issue__subcategories', function (Blueprint $table) {
+            $table->id();
+
+            $table->bigInteger('cat_id')->unsigned();
+            $table->bigInteger('issue_id')->unsigned();
+
+            $table->foreign('cat_id')->references('id')->on('subcategories')->onDelete('cascade');
+            $table->foreign('issue_id')->references('id')->on('issues')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('issue__subcategories');
+    }
+}
