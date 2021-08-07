b5add751cdcc6410f9bace6cbcd3cd6a6da43bbb
Oshada1988 <lahirupr471@gmail.com>
2021-08-08 01:13:09 +0530

    create many to many realtioship between issues and categories

--- Issue-Tracking-Systems/database/migrations/2021_08_07_191515_create_issue_categories_table.php
+++ Issue-Tracking-Systems/database/migrations/2021_08_07_191515_create_issue_categories_table.php
@@ -15,7 +15,10 @@ class CreateIssueCategoriesTable extends Migration
     {
         Schema::create('issue__categories', function (Blueprint $table) {
             $table->id();
-            $table->timestamps();
+            $table->integer('issue_id')->unsigned();
+            $table->integer('category_id')->unsigned();
+            $table->foreign('issue_id')->references('id')->on('issues');
+            $table->foreign('category_id')->references('id')->on('categories');
         });
     }
 
